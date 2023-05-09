95f577a8fd6d7f19de9ed5a3accfcd9f776bc65d
crowdbotics-apps <team@crowdbotics.com>
2023-05-09 17:45:55 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class M1(models.Model):
     'Generated Model'
     key = models.ForeignKey("users.User",on_delete=models.CASCADE,related_name="m1_key",)
+    int1 = models.BooleanField(null=True,blank=True,)
+    data1 = models.DateField(auto_now=True,null=True,blank=True,)
+    time1 = models.DateTimeField(null=True,blank=True,)
