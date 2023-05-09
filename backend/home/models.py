b4962ded00d1a59fd28adb7d89b2a5359f1ef7d6
crowdbotics-apps <team@crowdbotics.com>
2023-05-09 15:57:14 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,5 @@
+from django.conf import settings
 from django.db import models
+class M1(models.Model):
+    'Generated Model'
+    key = models.ForeignKey("users.User",on_delete=models.CASCADE,related_name="m1_key",)
