4350acf1a86511333683c032cdac9534a39e5e86
stevblag <stefan.v.blagojevic@gmail.com>
2023-05-09 12:20:03 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/sbtestAPI/m1s.slice.js
+++ store/sbtestAPI/m1s.slice.js
@@ -1,48 +1,36 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
-import { apiService } from "./api"
-export const api_v1_m1_list = createAsyncThunk(
-  "m1s/api_v1_m1_list",
-  async payload => {
-    const response = await apiService.api_v1_m1_list(payload)
-    return response.data
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { apiService } from "./api";
+export const api_v1_m1_list = createAsyncThunk("m1s/api_v1_m1_list", async payload => {
+  const response = await apiService.api_v1_m1_list(payload);
+  return response.data;
+});
+export const api_v1_m1_create = createAsyncThunk("m1s/api_v1_m1_create", async payload => {
+  const response = await apiService.api_v1_m1_create(payload);
+  return response.data;
+});
+export const api_v1_m1_retrieve = createAsyncThunk("m1s/api_v1_m1_retrieve", async payload => {
+  const response = await apiService.api_v1_m1_retrieve(payload);
+  return response.data;
+});
+export const api_v1_m1_update = createAsyncThunk("m1s/api_v1_m1_update", async payload => {
+  const response = await apiService.api_v1_m1_update(payload);
+  return response.data;
+});
+export const api_v1_m1_partial_update = createAsyncThunk("m1s/api_v1_m1_partial_update", async payload => {
+  const response = await apiService.api_v1_m1_partial_update(payload);
+  return response.data;
+});
+export const api_v1_m1_destroy = createAsyncThunk("m1s/api_v1_m1_destroy", async payload => {
+  const response = await apiService.api_v1_m1_destroy(payload);
+  return response.data;
+});
+const initialState = {
+  entities: [],
+  api: {
+    loading: "idle",
+    error: null
   }
-)
-export const api_v1_m1_create = createAsyncThunk(
-  "m1s/api_v1_m1_create",
-  async payload => {
-    const response = await apiService.api_v1_m1_create(payload)
-    return response.data
-  }
-)
-export const api_v1_m1_retrieve = createAsyncThunk(
-  "m1s/api_v1_m1_retrieve",
-  async payload => {
-    const response = await apiService.api_v1_m1_retrieve(payload)
-    return response.data
-  }
-)
-export const api_v1_m1_update = createAsyncThunk(
-  "m1s/api_v1_m1_update",
-  async payload => {
-    const response = await apiService.api_v1_m1_update(payload)
-    return response.data
-  }
-)
-export const api_v1_m1_partial_update = createAsyncThunk(
-  "m1s/api_v1_m1_partial_update",
-  async payload => {
-    const response = await apiService.api_v1_m1_partial_update(payload)
-    return response.data
-  }
-)
-export const api_v1_m1_destroy = createAsyncThunk(
-  "m1s/api_v1_m1_destroy",
-  async payload => {
-    const response = await apiService.api_v1_m1_destroy(payload)
-    return response.data
-  }
-)
-const initialState = { entities: [], api: { loading: "idle", error: null } }
+};
 const m1sSlice = createSlice({
   name: "m1s",
   initialState,
@@ -50,117 +38,108 @@ const m1sSlice = createSlice({
   extraReducers: {
     [api_v1_m1_list.pending]: (state, action) => {
       if (state.api.loading === "idle") {
-        state.api.loading = "pending"
+        state.api.loading = "pending";
       }
     },
     [api_v1_m1_list.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.entities = action.payload
-        state.api.loading = "idle"
+        state.entities = action.payload;
+        state.api.loading = "idle";
       }
     },
     [api_v1_m1_list.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.api.error = action.error
-        state.api.loading = "idle"
+        state.api.error = action.error;
+        state.api.loading = "idle";
       }
     },
     [api_v1_m1_create.pending]: (state, action) => {
       if (state.api.loading === "idle") {
-        state.api.loading = "pending"
+        state.api.loading = "pending";
       }
     },
     [api_v1_m1_create.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.entities.push(action.payload)
-        state.api.loading = "idle"
+        state.entities.push(action.payload);
+        state.api.loading = "idle";
       }
     },
     [api_v1_m1_create.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.api.error = action.error
-        state.api.loading = "idle"
+        state.api.error = action.error;
+        state.api.loading = "idle";
       }
     },
     [api_v1_m1_retrieve.pending]: (state, action) => {
       if (state.api.loading === "idle") {
-        state.api.loading = "pending"
+        state.api.loading = "pending";
       }
     },
     [api_v1_m1_retrieve.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.entities = [
-          ...state.entities.filter(record => record.id !== action.payload.id),
-          action.payload
-        ]
-        state.api.loading = "idle"
+        state.entities = [...state.entities.filter(record => record.id !== action.payload.id), action.payload];
+        state.api.loading = "idle";
       }
     },
     [api_v1_m1_retrieve.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.api.error = action.error
-        state.api.loading = "idle"
+        state.api.error = action.error;
+        state.api.loading = "idle";
       }
     },
     [api_v1_m1_update.pending]: (state, action) => {
       if (state.api.loading === "idle") {
-        state.api.loading = "pending"
+        state.api.loading = "pending";
       }
     },
     [api_v1_m1_update.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.entities = state.entities.map(record =>
-          record.id === action.payload.id ? action.payload : record
-        )
-        state.api.loading = "idle"
+        state.entities = state.entities.map(record => record.id === action.payload.id ? action.payload : record);
+        state.api.loading = "idle";
       }
     },
     [api_v1_m1_update.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.api.error = action.error
-        state.api.loading = "idle"
+        state.api.error = action.error;
+        state.api.loading = "idle";
       }
     },
     [api_v1_m1_partial_update.pending]: (state, action) => {
       if (state.api.loading === "idle") {
-        state.api.loading = "pending"
+        state.api.loading = "pending";
       }
     },
     [api_v1_m1_partial_update.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.entities = state.entities.map(record =>
-          record.id === action.payload.id ? action.payload : record
-        )
-        state.api.loading = "idle"
+        state.entities = state.entities.map(record => record.id === action.payload.id ? action.payload : record);
+        state.api.loading = "idle";
       }
     },
     [api_v1_m1_partial_update.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.api.error = action.error
-        state.api.loading = "idle"
+        state.api.error = action.error;
+        state.api.loading = "idle";
       }
     },
     [api_v1_m1_destroy.pending]: (state, action) => {
       if (state.api.loading === "idle") {
-        state.api.loading = "pending"
+        state.api.loading = "pending";
       }
     },
     [api_v1_m1_destroy.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.entities = state.entities.filter(
-          record => record.id !== action.meta.arg?.id
-        )
-        state.api.loading = "idle"
+        state.entities = state.entities.filter(record => record.id !== action.meta.arg?.id);
+        state.api.loading = "idle";
       }
     },
     [api_v1_m1_destroy.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.api.error = action.error
-        state.api.loading = "idle"
+        state.api.error = action.error;
+        state.api.loading = "idle";
       }
     }
   }
-})
+});
 export default {
   api_v1_m1_list,
   api_v1_m1_create,
@@ -169,4 +148,4 @@ export default {
   api_v1_m1_partial_update,
   api_v1_m1_destroy,
   slice: m1sSlice
-}
+};
